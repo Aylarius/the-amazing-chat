b6c711d159ec03076e92ed34674fc18bb1f1b154
Thifaine Noirault <thifaine.noirault@gmail.com>
2019-10-07 08:51:18 +0200

    Revert "fix: add style.css, move logic to server & other fix"

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const faker = require('faker');
-let loggedInUsers = [];
-let messages = [];
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var faker = require('faker');
+var loggedInUsers = [];
+var messages = [];
 
 function User(socketId, username, colour) {
     this.Id = socketId;
     this.Username = username;
     this.Colour = colour;
 }
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
@@ -21,50 +19,31 @@ io.on('connection', function(socket){
     let colour = '#' + Math.floor(Math.random()*16777215).toString(16);
     let user = new User(socket.id, faker.internet.userName(), colour);
 
+    loggedInUsers.push(user);
     socket.emit("connection", user);
-    if (!loggedInUsers.includes(user)) {
-        loggedInUsers.push(user);
-        io.emit("edit users", loggedInUsers);
-    }
+    socket.emit("edit users", loggedInUsers);
 
     socket.on('join', function (user) {
         socket.join(user.Username);
     });
 
     socket.on('message', function(msg){
-        switch (true) {
-            case msg.content.startsWith('/whoami'):
-                socket.emit("internal message", 'You are ' + user.Username);
-                break;
-            case msg.content.startsWith('/date'):
-                let date = new Date().toLocaleDateString('en-US');
-                socket.emit("internal message", 'The current date is ' + date);
-                break;
-            case msg.content.startsWith('/ping'):
-                io.to(socket.id).emit('ping received', 'pong');
-                break;
-            case msg.content.startsWith('@'):
-                let receiver = msg.content.split(" ")[0].replace("@", "");
-                let content = msg.content.replace("@"+receiver, "");
-                if (loggedInUsers.filter(user => user.Username === receiver).length === 1) {
-                    if (user.Username === receiver) {
-                        io.to(socket.id).emit('self message sent', receiver, content);
-                    } else {
-                        socket.broadcast.in(receiver).emit('private message received', user.Username, content);
-                        io.to(socket.id).emit('private message sent', receiver, content);
-                    }
-                }
-                break;
-            default:
-                io.emit("message", msg);
-        }
         messages.push(msg);
+        io.emit("message", msg);
     });
 
-    socket.on('disconnect', function() {
-        loggedInUsers = loggedInUsers.filter(user => user.Id !== socket.id);
-        io.emit("edit users", loggedInUsers);
+    socket.on('private message', function (sender, msg) {
+        socket.broadcast.in(sender).emit('private message received', sender, msg);
     });
+
+    socket.on('user ping', function () {
+        io.to(socket.id).emit('ping received', 'pong');
+    });
+});
+
+io.on('disconnect', function(socket) {
+    loggedInUsers = loggedInUsers.filter(user => user.Id !== socket.id);
+    socket.emit("edit users", loggedInUsers);
 });
 
 http.listen(3000, function () {
